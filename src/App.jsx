f16702539c0e26ab3da3bead73cacb03f7d3a491
Nikhil Sharma <nikhils.sharma2409@gmail.com>
2025-06-14 17:18:45 +0530

    Created

---
+++ src/App.jsx
@@ -0,0 +1,64 @@
+import { useState } from 'react'
+
+import './App.css'
+
+
+const allbrands=[
+  {id:1,Name:"maruti"},
+  {id:2,Name:"Mercedes"},
+  {id:3,Name:"Bemer"},
+  {id:4,Name:"MG"},
+  {id:5,Name:"Rolls Royce"},
+
+]
+
+
+function Filter(){
+  const [item,filteritem]=useState(allbrands);
+    const [notFound, setNotFound] = useState(false);
+
+
+  function change(e){
+    const x=e.target.value;
+    const filtered = allbrands.filter((brand) =>
+      brand.Name.toLowerCase().includes(x)
+    );
+   if (filtered.length > 0) {
+      filteritem(filtered);
+      setNotFound(false);
+    } else {
+      filteritem([]);
+      setNotFound(true);
+    }
+  }
+  return (
+    <>
+    <input type="text" onChange={change} />
+    {notFound ? (
+        <h2>Item not found</h2>
+      ) : (
+        <ul>
+          {item.map((brand) => (
+            <li key={brand.id}>{brand.Name}</li>
+          ))}
+        </ul>
+      )}
+    </>
+  )
+
+
+}
+
+function App() {
+
+
+  return (
+    <>
+    
+        <Filter/>
+      
+    </>
+  )
+}
+
+export default App
